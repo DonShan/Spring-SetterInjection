95c26454dbe49cea57c2ae462988de7518aa4ab2
Don Shan <msdjshan47@gmail.com>
2020-02-03 07:31:36 +0530

    SetterInection

---
+++ SpringSetterInjection/src/com/madushan/training/salesmanager/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.madushan.training.salesmanager.service;
+
+import java.util.List;
+
+
+
+import com.madushan.training.salesmanager.model.Employee;
+import com.madushan.training.salesmanager.repository.EmployeeRepository;
+import com.madushan.training.salesmanager.repository.HibernateEmployeeRepositoryImpl;
+
+
+@Service("employeeService")
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	//EmployeeRepository employeeRepository = new HibernateEmployeeRepositoryImpl();
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	
+	public EmployeeRepository getEmployeeRepository() {
+		return employeeRepository;
+	}
+
+
+
+	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.getAllEmployees();
+	}
+	
+
+}
